668079caeed3568d20b06265391c534f24c3179f
Juncheng <zhujuncheng1021@163.com>
2018-01-26 14:52:14 +0800

    update

--- Tutorial_Code/KnowFlow_03_AnalogRead_LED_about_input__output__v0.0/KnowFlow_03_AnalogRead_LED_about_input__output__v0.0.ino
+++ Tutorial_Code/KnowFlow_03_AnalogRead_LED_about_input__output__v0.0/KnowFlow_03_AnalogRead_LED_about_input__output__v0.0.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
+  Serial.begin(9600);     //  Set serial baud rate to 9600 bps
+  pinMode(A0,INPUT);      //  Set A0 to output
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  int val;
+  val=analogRead(0);      //  Read slider value from analog 0
+  Serial.println(val,DEC);//  Print the DEC value to serial port
+  delay(100);
+  if(val>20)
+  {
+    digitalWrite(13,HIGH);
+  }else{
+    digitalWrite(13,LOW);
+  }
 }
